fa20028499b1ce19246c39a6dd706a9d5461238b
thatbookshopguy <notthomasaustin@gmail.com>
2020-08-11 15:24:00 +0300

    blog slider added

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 };
+
+// слайдер
+
+var slides = document.querySelectorAll('.slider__item');
+var currentSlide = 0;
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = 'slider__item';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = 'slider__item showing';
+}
+
+next.onclick = function () {
+  nextSlide();
+};
+
+previous.onclick = function () {
+  previousSlide();
+};
+
+function exactSlide(n) {
+  currentSlide = n;
+  goToSlide(currentSlide);
+}
